78e250480e0545f4bd91031c8b5fbda479d67817
Sam Christiansen <sam@bonfirestudios.com>
2020-12-10 09:55:36 -0800

    baseline

---
+++ Assets/TestData/Bar.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "bar", menuName = "TestData/Bar")]
+public class Bar : ScriptableObject
+{
+    public int baz;
+}
---
+++ Assets/TestData/Foo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "foo", menuName = "TestData/Foo")]
+public class Foo : ScriptableObject, ISerializationCallbackReceiver
+{
+    public Bar bar;
+    public int baz;
+    public int boz;
+
+    public void OnBeforeSerialize()
+    {
+    }
+
+    public void OnAfterDeserialize()
+    {
+        if (bar != null)
+        {
+            baz = bar.baz;
+        }
+    }
+}
